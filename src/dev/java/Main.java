7c965abaf7d2c5753f1af9fb77d19a8a3ce4c1a0
admin <administrator@jits-3-5.jits.ipa.dc>
2022-12-22 10:34:51 +0600

    Add Hello world

---
+++ src/dev/java/Main.java
@@ -0,0 +1,8 @@
+package dev.java;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+    }
+}
