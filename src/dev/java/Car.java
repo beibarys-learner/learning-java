f44a8c9af12fe153df64fa2c6275ce75d6cc602e
admin <administrator@jits-3-5.jits.ipa.dc>
2022-12-22 10:46:25 +0600

    created car classes and object instances like bmw and nissan with model, name, color props

---
+++ src/dev/java/Car.java
@@ -0,0 +1,17 @@
+package dev.java;
+
+public class Car {
+    String name;
+    String model;
+    String color;
+
+    public void stop(){
+        String stop = "stop";
+        System.out.println("name + stop = " + name);
+    }
+    public void drive(){
+        String hello = "hello";
+        System.out.println("name + hello = " + name);
+    }
+
+}
--- src/dev/java/Main.java
+++ src/dev/java/Main.java
@@ -4,5 +4,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        Car nissan = new Car();
+        nissan.color = "blue";
+        nissan.name = "nissan n1";
+        nissan.model = "sedan";
+        nissan.drive();
+
+        Car bmw = new Car();
+        bmw.color = "blue";
+        bmw.name = "bmw x5";
+        bmw.model = "sedan";
+        bmw.stop();
     }
 }
